e6dfeca9f2e129286e111a4d429ebd853c319b71
Kumar0011agra <149942244+Kumar0011agra@users.noreply.github.com>
2024-04-05 23:01:24 +0530

    sudoers.py

--- BrandrdXMusic/plugins/sudo/sudoers.py
+++ BrandrdXMusic/plugins/sudo/sudoers.py
@@ -48,7 +48,7 @@ async def userdel(client, message: Message, _):
 @language
 async def sudoers_list(client, message: Message, _):
     if message.from_user.id not in SUDOERS:
-        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/BRANDED_WORLD'>🇷🇺⛦°𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚🇳</a>",
+        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/DARKY_X_CODER'>🇷🇺⛦°𝗕𝗥𝗔𝗡𝗗𝗘𝗗 ➤⃟⃪🇮⃪ 𓆩 ⃝⃪⃕🇩𝐚𝐫𝐤»➤⃟🇱𝐨𝐯𝐞𝐫๛⋆⏤‌❥︎⃟🦋</a>",
         disable_web_page_preview=True,
         parse_mode="html")
     text = _["sudo_5"]
